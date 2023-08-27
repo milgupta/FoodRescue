7651c2567fc1fc11f6168c126656c2c2d937551e
Milan Gupta <mguptop@icloud.com>
2023-08-27 10:32:58 -0700

    Initial Commit

---
+++ Food Rescue/Food_RescueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_RescueApp.swift
+//  Food Rescue
+//
+//  Created by Milan Gupta on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_RescueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
